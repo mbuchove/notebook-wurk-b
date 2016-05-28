f4dc4f53cabd0f9887b8e4c5e6822037df1b5be3
Matt Buchovecky <mbuchove@gmail.com>
2016-05-28 00:16:27 -0700

    added functions for binomial pricing model. draw lattices with stock prices or instrinsic option prices at nodes, and calculate call prices using the binomial formula

---
+++ R/binomial_pricing_model.R
@@ -0,0 +1,42 @@
+# hw 5 
+
+binomial_formula_call <- function(S0, E, u, d, r, n){
+  p <- (r-d)/(u-d)
+  pp <- (u/r)*p
+  print(p)
+  print(pp)
+  
+  k <- ceiling( log(E/S0, base=u) )
+  if (u^k == E/S0)
+    k <- k+1
+  k <- as.integer(ceiling( (n+k)/2) )
+  
+  j <- k:n
+  Cj <- choose(n, j) * ( S0 * pp^j * (1-pp)^(n-j) - E/(r)^n * p^j * (1-p)^(n-j) )
+  
+  sum(Cj)
+  
+} # binomial_formula 
+
+S0 <- 50.00 
+E <- 60.00
+u <- 1.2
+d <- 1. / u
+r <- 1.1
+#r <- 0.1
+n <- 10
+
+C <- binomial_formula_call(S0=S0, E=E, u=u, d=d, r=r, n=n)
+print(C)
+
+p <- (r-d)/(u-d)
+pp <- (u/r)*p
+
+k <- 6 
+j <- k:n
+s <- 2*j - n
+iV <- S0 * u^s - E
+eVs <- choose(n,j) * p^j * (1-p)^(n-j) * iV
+eV <- sum(eVs)
+eV / r^n
+
---
+++ R/draw_lattice.R
@@ -0,0 +1,72 @@
+gen_lattice <- function(S0=100, u=1.1, d=.9, N=3, E=0, option='call') {
+  S <- c()
+  S[1] <- S0
+  count <- 2
+  
+  for (i in 1:N) {
+    for (j in 0:i) {
+      S[count] <- S0 * d^j * u^(i-j) - E
+      if (option == 'put')
+        S[count] <- -1 * S[count]
+      if (S[count] < 0)
+        S[count] <- 0 
+      #S[count] <- S0 * u^j * d^(i-j)
+      count <- count + 1
+    } # for loop over nodes in time step 
+  } # for loop over time steps 
+  return(S)
+} # gen_lattice
+
+dot_lattice <- function(S, labels=FALSE) {
+  shape <- ifelse(labels == TRUE, "plaintext", "point")
+  
+  cat("digraph G {", "\n", sep="")
+  cat("node[shape=",shape,"];","\n", sep="")
+  #cat("node[shape=",shape,", samehead, sametail];","\n", sep="")
+  cat("rankdir=LR;","\n")
+  
+  cat("edge[arrowhead=none];","\n")
+  
+  # Create a dot node for each element in the lattice
+  for (i in 1:length(S)) {
+    cat("node", i, "[label=\"", S[i], "\"];", "\n", sep="")
+  }
+  
+  # The number of levels in a binomial lattice of length N
+  # is `$\frac{\sqrt{8N+1}-1}{2}$`
+  L <- ((sqrt(8*length(S)+1)-1)/2 - 1)
+  
+  k<-1
+  for (i in 1:L) {
+    tabs <- rep("\t",i-1)
+    j <- i
+    while(j>0) {
+      cat("node",k,"->","node",(k+i),";\n",sep="")
+      cat("node",k,"->","node",(k+i+1),";\n",sep="")
+      k <- k + 1
+      j <- j - 1
+    }
+  } # loop over levels 
+  
+  cat("}", sep="")
+} # dot_lattice
+
+lat_10 <- capture.output(dot_lattice(gen_lattice(N=10, u=1.2, d=1./1.2)))
+cat(lat_10, file="/Users/mbuchove/Dropbox/Physics/ProbabilityStatistics/Stats_C283/lattice_10_nolabel.dot")
+
+lat_10_lab <- capture.output(dot_lattice(gen_lattice(S0=50, N=10, u=1.2, d=1./1.2), labels=TRUE))
+cat(lat_10_lab, file="/Users/mbuchove/Dropbox/Physics/ProbabilityStatistics/Stats_C283/lattice_10_label.dot")
+
+lat_prices <- capture.output(dot_lattice(gen_lattice(S0=50, N=3, u=1.06, d=0.95, E=0), labels=TRUE))
+cat(lat_prices, file="/Users/mbuchove/Dropbox/Physics/ProbabilityStatistics/Stats_C283/bpm_lattice_3_price.dot")
+
+lat_ivals_call <- capture.output(dot_lattice(gen_lattice(S0=50, N=3, u=1.06, d=0.95, E=51.00), labels=TRUE))
+cat(lat_ivals_call, file="/Users/mbuchove/Dropbox/Physics/ProbabilityStatistics/Stats_C283/bpm_lattice_3_ivals_call.dot")
+
+lat_ivals_put <- capture.output(dot_lattice(gen_lattice(S0=50, N=3, u=1.06, d=0.95, E=51.00, option='put'), labels=TRUE))
+cat(lat_ivals_put, file="/Users/mbuchove/Dropbox/Physics/ProbabilityStatistics/Stats_C283/bpm_lattice_3_ivals_put.dot")
+
+lat_ivals_put
+
+# process dot files with 
+# dot -Tpng -o lattice_10_nolabel.png -v lattice_10_nolabel.dot 
